456058eef96d34446b3de41c728855386fded061
spanozzo <spanozzo>
2019-04-12 19:01:28 +0200

    Commenti in parte aggiunti, rimangono dei dubbi. Ho lasciato alcuni commenti che avevi messo tu, così dai una controllata finale e in caso correggi quel che ho scritto

--- HM2/G29HM2.py
+++ HM2/G29HM2.py
@@ -10,7 +10,6 @@ parser.add_argument('-f', '--file_path', help='path/to/file.txt', required=True)
 parser.add_argument('-k', '--k', help='number of partitions', required=True)
 args = vars(parser.parse_args())
 
-# K = 3  # number of partitions
 path = os.path.join(os.getcwd(), args['file_path'])
 if not os.path.isfile(path):
     raise EnvironmentError('Path/to/file.txt is not right')
@@ -20,6 +19,8 @@ spark_conf = SparkConf(True).setAppName('G29HM2').setMaster('local')
 sc = SparkContext(conf=spark_conf)
 
 
+# scan the document and add the new word in the dictionary if it's a new one,
+# otherwise increment the count of that word by one
 def word_count_per_doc(document):
     pairs_dict = {}  # dictionary to store (key, value) pairs
     for word in document.split(' '):
@@ -30,6 +31,8 @@ def word_count_per_doc(document):
     return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
 
 
+# non ho capito la descrizione, anche perchè couple non viene utilizzato,
+# quindi immagino sia riferita ad una versione precedente
 def assign_random_keys(couple):
     """
     Assigns a random key value in range [0, K) to couple. There is no need to return it
@@ -41,14 +44,17 @@ def assign_random_keys(couple):
     return int(K*random())
 
 
+# assignment 1: the Improved Word count 1 algorithm described in class the using reduceByKey method.
+# flatMap produce the new RDD applying word_count_per_doc function.
+# The reduceByKey is the transformation that aggregate data corresponding to the key (word) with the help
+# of an associative reduce function (add).
+# The count function count the number of distinct word in all documents.
 def word_count_1():
-    # assignment 1: the Improved Word count 1 algorithm described in class the using reduceByKey method
     # reduceByKey, da quello che ho capito, prende la lista di coppie passata da flatMap, e le raggruppa per chiave.
     # e ti chiede cosa fare dei valori delle chiavi: in questo caso dobbiamo sommarle.
     # Di conseguenza, i valori che prende "add" in input sono il valore della chiave della coppia che esiste gia' e
     # il valore della chiave della coppia che vogliamo inserire.
 
-    # per vedere la documentazione dei metodi, pigia "CTRL" + "Q"
     wc_in_doc = docs\
         .flatMap(word_count_per_doc)\
         .reduceByKey(add)\
@@ -56,6 +62,7 @@ def word_count_1():
     return wc_in_doc
 
 
+#
 def gather_pairs(x):
     """
     Generate a new stream as a list
@@ -65,9 +72,6 @@ def gather_pairs(x):
     """
     couples = x[1]
     new_couples = {}
-    # new_couples = {
-    #   word: count
-    # }
     for word, count in couples:
         if word in new_couples.keys():
             new_couples[word] += count
@@ -77,10 +81,18 @@ def gather_pairs(x):
         yield word, count
 
 
+#
 def gather_pairs_partitions(couples):
     return gather_pairs(('', couples))
 
 
+# assignment 2.1: the Improved Word count 2 algorithm where random keys take K possible values, where K is the value given in input.
+# flatMap produce the new RDD applying word_count_per_doc function.
+# the groupBy aggregate data using a random number created using the function "assign_random_key".
+# flatMap (ho capito solo a grandi linee cosa fa gather_pairs, quindi non riesco a dare una spiegazione precisa).
+# The reduceByKey is the transformation that aggregate data corresponding to the key (word) with the help
+# of an associative reduce function (add).
+# The count function count the number of distinct word in all documents.
 def word_count_2():
     wc_in_doc = docs\
         .flatMap(word_count_per_doc)\
@@ -88,10 +100,15 @@ def word_count_2():
         .flatMap(gather_pairs)\
         .reduceByKey(add)\
         .count()
-
     return wc_in_doc
 
 
+# assignment 2.2: the Improved Word count 2 algorithm that exploits the subdivision of docs into K parts and access each partition separately.
+# flatMap produce the new RDD applying word_count_per_doc function.
+# stesso dubbio di prima, però su gather_pairs_partitions
+# The reduceByKey is the transformation that aggregate data corresponding to the key (word) with the help
+# of an associative reduce function (add).
+# Non mi ricordo di preciso perchè in questa funzione ritorniamo l'RDD trasformato e non il valore del count() direttamente come prima
 def word_count_2_with_partition():
     wc_in_doc = docs\
         .flatMap(word_count_per_doc)\
@@ -101,6 +118,7 @@ def word_count_2_with_partition():
     return wc_in_doc
 
 
+#
 def print_stats(k=K):
     start = time()
     print(
@@ -127,6 +145,7 @@ def print_stats(k=K):
     end = time()
     print('Time elapsed: {}\n#--------------------------------------------------#\n'.format(end - start))
 
+    # assignment 3: calculate the average length of distinct words in all the documents
     print('Average length of distinct words: {}'.format(
         wc_partition
         .map(lambda x: len(x[0]))
